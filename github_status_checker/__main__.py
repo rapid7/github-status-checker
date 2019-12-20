36d152127e57e17a6c2d5083d6172a09153dd7ce
Mike Rinehart <mike_rinehart@rapid7.com>
2019-12-20 11:58:33 -0600

    Refine available choices

--- github_status_checker/__main__.py
+++ github_status_checker/__main__.py
@@ -1,23 +1,8 @@
-from github_status_checker.models.summary import Summary
-from github_status_checker.models.component import Component
-from github_status_checker.api import API
-from pkg_resources import get_distribution
-
 import argparse
 
+from pkg_resources import get_distribution
 
-def _get_component(component: str) -> Component:
-    api = API.new()
-    summary: Summary = api.get_summary()
-
-    for c in summary.components:
-        if component in c.name.lower():
-            return c
-
-
-def check(component: str) -> None:
-    comp: Component = _get_component(component=component)
-    print(f"{comp.name} is currently {comp.status.value}!")
+from github_status_checker.controllers.commands import Commands
 
 
 def main():
@@ -27,12 +12,13 @@ def main():
 
     argparser.add_argument("component",
                            help="Component to check",
-                           choices=["webhooks", "api"],
+                           choices=["webhooks","actions"],
                            action="store",
                            type=str)
 
     args = argparser.parse_args()
-    check(component=args.component)
+    Commands.check(component=args.component)
+
 
 if __name__ == "__main__":
     main()
---
+++ github_status_checker/controllers/commands.py
@@ -0,0 +1,20 @@
+from github_status_checker.api import API
+from github_status_checker.models.component import Component
+from github_status_checker.models.summary import Summary
+
+
+class Commands(object):
+
+    @staticmethod
+    def _get_component(component: str) -> Component:
+        api = API.new()
+        summary: Summary = api.get_summary()
+
+        for c in summary.components:
+            if component in c.name.lower().split(" "):
+                return c
+
+    @staticmethod
+    def check(component: str) -> None:
+        comp: Component = Commands._get_component(component=component)
+        print(f"{comp.name} is currently {comp.status.value}!")
