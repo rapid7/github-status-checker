0d1988e1e1ee29f556c2f8e947aab6adae190e54
Mike Rinehart <mike_rinehart@rapid7.com>
2019-12-20 11:43:13 -0600

    Preliminary

--- github_status_checker/__main__.py
+++ github_status_checker/__main__.py
@@ -1,24 +1,38 @@
-import json
-
-import click
-import requests
-
 from github_status_checker.models.summary import Summary
+from github_status_checker.models.component import Component
+from github_status_checker.api import API
+from pkg_resources import get_distribution
+
+import argparse
 
 
-@click.option("--name", envvar="some_awesome_name")
+def _get_component(component: str) -> Component:
+    api = API.new()
+    summary: Summary = api.get_summary()
+
+    for c in summary.components:
+        if component in c.name.lower():
+            return c
+
+
+def check(component: str) -> None:
+    comp: Component = _get_component(component=component)
+    print(f"{comp.name} is currently {comp.status.value}!")
+
+
 def main():
-    pass
+    version_string = get_distribution('insightconnect-integrations-validators')
+    argparser = argparse.ArgumentParser(epilog=version_string,
+                                        description="Status checker for GitHub")
 
+    argparser.add_argument("component",
+                           help="Component to check",
+                           choices=["webhooks", "api"],
+                           action="store",
+                           type=str)
+
+    args = argparser.parse_args()
+    check(component=args.component)
 
 if __name__ == "__main__":
-    ENDPOINT = "https://kctbh9vrtdwd.statuspage.io/api/v2"
-
-    url = f"{ENDPOINT}/summary.json"
-
-    resp = requests.get(url)
-
-    j = json.dumps(resp.json(), default=lambda o: o.__dict__)
-    summary: Summary = Summary.from_json(json_=resp.json())
-    for c in summary.components:
-        print(f"{c.name} is currently {c.status.value}")
+    main()
